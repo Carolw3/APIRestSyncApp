6269717b64c3d276953892be91e920416ae1aceb
JordiIbanezMasip <jordimasip2003@gmail.com>
2026-01-24 11:00:51 +0100

    schema

---
+++ sync-api/src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE sync_item (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    description TEXT,
+    puntuacion INT NOT NULL,
+    favoritos BOOLEAN,
+    imagen_doc VARCHAR(200),
+    imagen_per VARCHAR(200),
+    categoria VARCHAR(100) NOT NULL
+);
